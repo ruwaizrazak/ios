af6d5c19577ec190ce4b0530fbec2fa2bdbae4db
Ruwaiz <ruwaizrazak@gmail.com>
2020-08-14 12:26:22 +0530

    completed section 3

---
+++ Im Rich/Im Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Im Rich
+//
+//  Created by Ruwaiz Razak on 11/08/20.
+//  Copyright © 2020 Ruwaiz Razak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ImPoor/ImPoor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImPoor
+//
+//  Created by Ruwaiz Razak on 14/08/20.
+//  Copyright © 2020 Ruwaiz Razak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
