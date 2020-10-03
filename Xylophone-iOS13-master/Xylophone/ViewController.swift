565db40ae2c5f265943f4de3b437eb068e65b5c1
Ruwaiz <ruwaizrazak@gmail.com>
2020-10-03 13:36:21 +0530

    learning about how to play sound using swift.
    Learned about,
    - connecting multiple buttons to single IB action
    - using AVfoundation
    - tweaking actions

--- Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+var player: AVAudioPlayer?
+let seconds = 0.2
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func Button(_ sender: UIButton) {
+        sender.alpha=0.5
+        playSound(tone  :  sender.currentTitle!)
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            // Put your code which should be executed with a delay here
+            sender.alpha=1
+        }
+        
+    }
+    
+    func playSound( tone: String) {
+        guard let url = Bundle.main.url(forResource: tone, withExtension: "wav") else { return }
 
-
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            guard let player = player else { return }
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
     
 
 }
