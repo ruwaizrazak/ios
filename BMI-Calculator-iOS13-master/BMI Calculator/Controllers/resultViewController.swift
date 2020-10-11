7b85abf39e3c12028cf632ef75a42d267f864995
Ruwaiz <ruwaizrazak@gmail.com>
2020-10-11 16:32:23 +0530

    created another viwe controller for the second screen

---
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/resultViewController.swift
@@ -0,0 +1,32 @@
+//
+//  resultViewController.swift
+//  BMI Calculator
+//
+//  Created by Ruwaiz Razak on 11/10/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class resultViewController: UIViewController {
+    @IBOutlet weak var finalBMI: UILabel!
+    @IBOutlet weak var motivationLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
