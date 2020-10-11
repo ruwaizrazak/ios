910d4024c63cfa4741de0b6e09940347ab50145d
Ruwaiz <ruwaizrazak@gmail.com>
2020-10-11 16:15:04 +0530

    adding a new view controller for the second screen

---
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/seecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  seecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Ruwaiz Razak on 11/10/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
